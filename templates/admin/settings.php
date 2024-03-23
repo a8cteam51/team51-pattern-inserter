1508c4ab8f8fe47e57ac5ec98101157996d64deb
Nate Allen <nate.allen@automattic.com>
2024-03-23 17:24:56 -0500

    Make the settings page match the default settings

--- templates/admin/settings.php
+++ templates/admin/settings.php
@@ -9,12 +9,23 @@
 <div class="wrap">
 	<h1><?php esc_html_e( 'Pattern Inserter Settings', 'wpcomsp-team51-pattern-inserter' ); ?></h1>
 
-	<form method="post">
-
-		<p><?php echo esc_html__( 'Last updated: ', 'wpcomsp-team51-pattern-inserter' ) . esc_html( $options['last_update'] ); ?></p>
-		<p><?php echo esc_html__( 'Number of patterns: ', 'wpcomsp-team51-pattern-inserter' ) . esc_html( $options['num_patterns'] ); ?></p>
+	<form method="post" action="<?php echo esc_url( $_SERVER['REQUEST_URI'] ); ?>">
+		<table class="form-table">
+			<tbody>
+			<tr>
+				<th scope="row"><label for="last_updated"><?php echo esc_html__( 'Last updated:', 'wpcomsp-team51-pattern-inserter' ); ?></label></th>
+				<td><span id="last_updated"><?php echo esc_html( $options['last_update'] ); ?></span></td>
+			</tr>
+			<tr>
+				<th scope="row"><label for="num_patterns"><?php echo esc_html__( 'Number of patterns:', 'wpcomsp-team51-pattern-inserter' ); ?></label></th>
+				<td><span id="num_patterns"><?php echo esc_html( $options['num_patterns'] ); ?></span></td>
+			</tr>
+			</tbody>
+		</table>
 
 		<?php wp_nonce_field( 'clear_cache', 'clear_cache_nonce' ); ?>
-		<button type="submit" name="clear_cache">Clear cache</button>
+		<p class="submit">
+			<input type="submit" name="clear_cache" class="button button-primary" value="Clear cache">
+		</p>
 	</form>
 </div>
